e3e3295f7452fd79b7fbb263d18049fc5c5497ec
mohdidr12 <mohd.idris@globallogic.com>
2022-08-05 11:01:22 +0530

    Add project files.

---
+++ Day 13/StudentManagement/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    //        StudentManagementIO studentManagementIO = new StudentManagementIO();
+    //        Boolean toExit = true;
+    //        do
+    //        {
+    //            switch(studentManagementIO.Menu())
+    //            {
+    //                case 1:
+    //                    {
+    //                        studentManagementIO.AddStudent();
+    //                    }
+    //                case 2:
+    //                    {
+    //                        studentManagementIO.DisplayStudent();
+    //                        break;
+    //                    }
+    //                case 3:
+    //                    {
+    //                        studentManagementIO.UpdateStudent();
+    //                        break;
+    //                    }
+    //                case 4:
+    //                    {
+    //                        studentManagementIO.DeleteStudent();
+    //                        break;
+    //                    }
+    //                case 0:
+    //                    {
+    //                        toExit = false;
+    //                        break;
+    //                    }
+    //                default:
+    //                    {
+    //                        Console.WriteLine("enter valid option");
+    //                        break;
+    //                    }
+    //            }
+    //        }
+
+        }
+    }
+}
